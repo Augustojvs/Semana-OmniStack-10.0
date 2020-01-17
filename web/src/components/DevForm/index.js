0ef4f230787023f23887b9acb0e07d2c37761922
João Pedro <joaopedrocj@gmail.com>
2020-01-17 09:51:34 -0300

    desestruturação do onSubmit no DevForm

--- web/src/components/DevForm/index.js
+++ web/src/components/DevForm/index.js
@@ -2,69 +2,69 @@ import React, { useState, useEffect } from 'react';
 
 //import './styles.css';
 
-function DevForm( onSubmit ) {
-    const [github_username, setGithubUsername] = useState('');
-    const [techs, setTechs] = useState('');
-    const [latitude, setLatitude] = useState('');
-    const [longitude, setLongitude] = useState('');
+function DevForm({ onSubmit }) {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const { latitude, longitude } = position.coords;
-    
-              setLatitude(latitude);
-              setLongitude(longitude);
-          },
-          (err) => {
-            console.log(err);
-          },
-          {
-            timeout: 30000,
-          }
-        )
-      }, []);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
 
-      async function handleSubmit(e) {
-        e.preventDefault();
-
-          await onSubmit({
-            github_username,
-            techs,
-            latitude,
-            longitude,
-        });
-
-        setGithubUsername('');
-        setTechs('');
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
       }
+    )
+  }, []);
 
-    return(
-      <form onSubmit={handleSubmit}>
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    await onSubmit({
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    });
+
+    setGithubUsername('');
+    setTechs('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-block">
+        <label htmlFor="gitbug_username">Usuário do Github</label>
+        <input name="gitbug_username" id="gitbug_username" value={github_username} onChange={e => setGithubUsername(e.target.value)} required />
+      </div>
+
+      <div className="input-block">
+        <label htmlFor="techs">Técnologias</label>
+        <input name="techs" id="techs" value={techs} onChange={e => setTechs(e.target.value)} required />
+      </div>
+
+      <div className="input-group">
         <div className="input-block">
-          <label htmlFor="gitbug_username">Usuário do Github</label>
-          <input name="gitbug_username" id="gitbug_username" value={github_username} onChange={e => setGithubUsername(e.target.value)} required/>
+          <label htmlFor="latitude">Latitude</label>
+          <input type="number" name="latitude" id="latitude" value={latitude} onChange={e => setLatitude(e.target.value)} required />
         </div>
 
         <div className="input-block">
-          <label htmlFor="techs">Técnologias</label>
-          <input name="techs" id="techs" value={techs} onChange={e => setTechs(e.target.value)} required/>
+          <label htmlFor="longitude">Longitude</label>
+          <input type="number" name="longitude" id="longitude" value={longitude} onChange={e => setLongitude(e.target.value)} required />
         </div>
-
-        <div className="input-group">
-          <div className="input-block">
-            <label htmlFor="latitude">Latitude</label>
-            <input type="number" name="latitude" id="latitude" value={latitude} onChange={e => setLatitude(e.target.value)} required/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="longitude">Longitude</label>
-            <input type="number" name="longitude" id="longitude" value={longitude} onChange={e => setLongitude(e.target.value)} required/>
-          </div>       
-        </div>  
-        <button type="submit">Salvar</button> 
-      </form>
-      );
+      </div>
+      <button type="submit">Salvar</button>
+    </form>
+  );
 }
 
 export default DevForm;
